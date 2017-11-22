6426ae07e40b49f06bb8cbfc03bf46a55b3b7b32
Chance Phiniezy <phiniezyc@gmail.com>
2017-11-22 17:04:27 -0500

    added handlebars and bodyparser middleware. As well as told express where to look for static content-inside public folder

--- server.js
+++ server.js
@@ -1,1 +1,21 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const exphbs = require("express-handlebars");
+
+//creates the express server for node
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+
+
+
