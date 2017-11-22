9c6b95e7089ee18c51e66127e73663449170350e
Chance Phiniezy <phiniezyc@gmail.com>
2017-11-22 18:23:41 -0500

    added in 3 test routes that just render HTML to the page or redirect to the home screen

--- controllers/htmlRoutes.js
+++ controllers/htmlRoutes.js
@@ -4,14 +4,25 @@ const path = require("path");
 
 
 module.exports = (app) => {
-    app.get("/", function (req, res) {
+    app.get("/", (req, res) => {
         //res.sendFile(path.join(__dirname, "../public/survey.html"));
         res.send("Hello Test!");
+    });
+
+
+    app.get("/test", (req, res) => {
+        //res.sendFile(path.join(__dirname, "../public/survey.html"));
+        res.send("Testing Test route!");
+        
     });
 
     // If no matching route is found, we default to home
     app.get("*", (req, res) => {
         //res.sendFile(path.join(__dirname, "../public/home.html"));
+        //res.send("this is the catch all route");
+        res.redirect("/");
     });
+
+
 
 };
