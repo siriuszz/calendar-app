cd46e735b93783a26fd802c090c51584476241ff
Chance Phiniezy <phiniezyc@gmail.com>
2017-12-01 02:58:01 -0500

    required db from models file in apiRouts.js file

--- controllers/apiRoutes.js
+++ controllers/apiRoutes.js
@@ -1,13 +1,13 @@
-
 const path = require("path");
 
+const db = require("../models");
 
 
 
 module.exports = (app) => {
 
 
-    
+
     app.get("/api/userDashboard", (req, res) => {
         //res.json(friends);
         console.log("test");
@@ -15,18 +15,30 @@ module.exports = (app) => {
         res.json(newEvent);
     });
 
-    
 
-    app.post("/api/updateEvents", (req, res) =>{
-        
+
+    app.post("/api/updateEvents", (req, res) => {
+
         console.log("testing api/updateEvents");
         console.log(req.body);
 
         res.json(req.body);
-        
+
 
         // req.json(newEvent);
         // req.end();
-        
+
     });
 };
+
+// app.post("/api/posts", function(req, res) {
+//     console.log(req.body);
+//     db.Post.create({
+//       title: req.body.title,
+//       body: req.body.body,
+//       category: req.body.category
+//     })
+//     .then(function(dbPost) {
+//       res.json(dbPost);
+//     });
+//   });
