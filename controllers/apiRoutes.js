fd17c1adb59c36afc52ad0211215563d0ec82e69
Chance Phiniezy <phiniezyc@gmail.com>
2017-12-01 04:37:23 -0500

    cleaned up uncessary comment

--- controllers/apiRoutes.js
+++ controllers/apiRoutes.js
@@ -31,14 +31,3 @@ module.exports = (app) => {
     });
 };
 
-// app.post("/api/posts", function(req, res) {
-//     console.log(req.body);
-//     db.Post.create({
-//       title: req.body.title,
-//       body: req.body.body,
-//       category: req.body.category
-//     })
-//     .then(function(dbPost) {
-//       res.json(dbPost);
-//     });
-//   });
