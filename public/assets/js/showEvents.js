ead83bc9aeb2faf41ac2c458804677e0ad92e1f3
Chance Phiniezy <phiniezyc@gmail.com>
2017-12-02 15:57:20 -0500

    appends title to the display section: working, but need to add more

--- public/assets/js/showEvents.js
+++ public/assets/js/showEvents.js
@@ -1,49 +1,61 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
-const displayEventsSection = $("#displayAllEventsSection");
-
-
-$.get("/api/events/", function (data) {
-    //console.log(data.id);
-
-    if (data) {
-        // If this post exists, prefill our cms forms with its data
-        //displayEventsSection.val(data.newEventNameInput);
-        //displayEventsSection.val(data.title);
-
-        //console.log(displayEventsSection.val(data.newEventNameInput));
-        //console.log(displayEventsSection.val(data.title));
-
-        // $("#displayEventsSection").html(data.newEventNameInput);
-        //$("#displayEventsSection").text(data);
-        console.log("testing this out",data);
-        //console.log(data.body.title);
-        
-
-        
+    const displayEventsSection = $("#displayAllEventsSection");
 
 
+    $.get("/api/events/", function (data) {
+        //console.log(data.id);
+        $("#displayAllEventsSection").append("<h1>" + data[0].title );
 
-        // bodyInput.val(data.body);
-        // postCategorySelect.val(data.category);
-        // // If we have a post with this id, set a flag for us to know to update the post
-        // // when we hit submit
-        // updating = true;
-    }
-});
+        const displayData = data;
+        displayData.forEach((object) => {
+            $("#displayAllEventsSection").append("<h1>" + object.title );
+        });
+       //$("#displayAllEventsSection").append("<h1>" + data[0].title );
+            
+        if (data) {
+            // If this post exists, prefill our cms forms with its data
+            //displayEventsSection.val(data.newEventNameInput);
+            //displayEventsSection.val(data.title);
 
-$("#goToAddEventsPageButton").on("click", ((event) => {
-    event.preventDefault();
+            //console.log(displayEventsSection.val(data.newEventNameInput));
+            //console.log(displayEventsSection.val(data.title));
+
+            //$("#displayEventsSection").html(data.title);
+            $("#displayAllEventsSection").append(data);
+            console.log("testing this out", data);
+
+
+            console.log(data[0].title);
 
 
 
 
 
 
-    window.location.replace('/userDashboard');
 
 
 
-}));
+            // bodyInput.val(data.body);
+            // postCategorySelect.val(data.category);
+            // // If we have a post with this id, set a flag for us to know to update the post
+            // // when we hit submit
+            // updating = true;
+        }
+    });
+
+    $("#goToAddEventsPageButton").on("click", ((event) => {
+        event.preventDefault();
+
+
+
+
+
+
+        window.location.replace('/userDashboard');
+
+
+
+    }));
 
 });
