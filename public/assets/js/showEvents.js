94ba8d244e3e1af55447fb40010d6565b4191191
Chance Phiniezy <phiniezyc@gmail.com>
2017-12-02 10:30:05 -0500

    pushed data to database. Working. not perfect, but works.  Need to fix where data displays to page

--- public/assets/js/showEvents.js
+++ public/assets/js/showEvents.js
@@ -1,4 +1,23 @@
+const displayEventsSection = $("#displayAllEventsSection");
 
+
+$.get("/api/events/", function (data) {
+    if (data) {
+        // If this post exists, prefill our cms forms with its data
+        displayEventsSection.val(data.newEventNameInput);
+        displayEventsSection.val(data.title);
+
+        console.log(displayEventsSection.val(data.newEventNameInput));
+        console.log(displayEventsSection.val(data.title));
+
+
+        // bodyInput.val(data.body);
+        // postCategorySelect.val(data.category);
+        // // If we have a post with this id, set a flag for us to know to update the post
+        // // when we hit submit
+        // updating = true;
+    }
+});
 
 $("#goToAddEventsPageButton").on("click", ((event) => {
     event.preventDefault();
@@ -6,12 +25,13 @@ $("#goToAddEventsPageButton").on("click", ((event) => {
 
 
 
-    
+
 
     window.location.replace('/userDashboard');
 
 
-    
+
 }));
+
 
 
