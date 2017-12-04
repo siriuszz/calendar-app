ae0991bf6b1e29acaeabf0f49235cc58cc8e0ac7
Chance Phiniezy <phiniezyc@gmail.com>
2017-12-04 02:11:50 -0500

    added for each function that appends each Event to the page

--- public/assets/js/showEvents.js
+++ public/assets/js/showEvents.js
@@ -1,15 +1,15 @@
 $(document).ready(function () {
 
-    const displayEventsSection = $("#eventsDisplay");
-    const newEventDisplayPanel = $("<div>");
+    // const displayEventsSection = $("#eventsDisplay");
+    // const newEventDisplayPanel = $("<div>");
 
-    const newEventTitlePanel = $("<h2>");
+    // const newEventTitlePanel = $("<h2>");
 
-    const newEventDatePanel = $("<small>");
+    // const newEventDatePanel = $("<small>");
 
-    const newEventDescriptionPanel = $("<p>");
+    // const newEventDescriptionPanel = $("<p>");
 
-    const emptyEventsContainer = [];
+    // const emptyEventsContainer = [];
 
     $.get("/api/events/", function (data) {
         //console.log(data.id);
@@ -44,18 +44,41 @@ $(document).ready(function () {
         //     // $("#eventsDisplay").append("<h3>" + object.description).css("font-size", "50px");
         // });
 
-        for (var i = 0; i < data.length; i++) {
+        // for (var i = 0; i < data.length; i++) {
 
-            var row = $("<div>");
-            row.addClass("event");
+        //     var eventsSection = $("<div>");
+        //     eventsSection.addClass("event");
 
-            row.append("<p>" + data[i].title + " event.. </p>");
-            row.append("<p>" + data[i].date + "</p>");
-            // row.append("<p>At " + moment(data[i].created_at).format("h:mma on dddd") + "</p>");
+        //     eventsSection.append("<h2>" + data[i].title + " event.. </p>");
+        //     eventsSection.append("<h3>" + data[i].date + "</p>");
+        //     eventsSection.append("<p>" + data[i].description + "</p>");
+        //     //eventsSection.append("<p>At " + moment(data[i].date).format("h:mma on dddd") + "</p>");
 
-            $("#eventsDisplay").append(row);
+        //     $("#eventsDisplay").append(eventsSection);
 
-        }
+        // }
+
+
+        // function createEventSection() {
+        //     var eventsSection = $("<div>");
+        //     eventsSection.addClass("event");
+
+        //     eventsSection.append("<p>" + data.title + " event.. </p>");
+        //     eventsSection.append("<p>" + data.date + "</p>");
+        //     eventsSection.append("<p>" + data.description + "</p>");
+        // }
+
+        displayData.forEach((object) => {
+            var eventsSection = $("<div>");
+            eventsSection.addClass("event");
+
+            eventsSection.append("<h2>" + object.title + " event.. </p>");
+            eventsSection.append("<h3>" + object.date + "</p>");
+            eventsSection.append("<p>" + object.description + "</p>");
+
+            $("#eventsDisplay").append(eventsSection);
+
+        });
 
 
         if (data) {
