a4e2b1320ac5cdbe6ff293ef00f825b21ed84dbf
Chance Phiniezy <phiniezyc@gmail.com>
2017-12-04 01:28:40 -0500

    appending events to page. Works but need to format

--- public/assets/js/showEvents.js
+++ public/assets/js/showEvents.js
@@ -1,23 +1,67 @@
 $(document).ready(function () {
 
     const displayEventsSection = $("#eventsDisplay");
+    const newEventDisplayPanel = $("<div>");
+
+    const newEventTitlePanel = $("<h2>");
+
+    const newEventDatePanel = $("<small>");
+
+    const newEventDescriptionPanel = $("<p>");
+
+    const emptyEventsContainer = [];
 
     $.get("/api/events/", function (data) {
         //console.log(data.id);
-        $("#eventsDisplay").append("<h1>" + data[0].title);
+        // $("#eventsDisplay").append("<h1>" + data[0].title);
+
+        // function showEvents(object) {
+
+
+        //     // $("#eventsDisplay").append("<br>");
+        //     // $("#eventsDisplay").append("<h2>" + object.title);
+        //     // $("#eventsDisplay").append("<h3>" + object.date);
+        //     // $("#eventsDisplay").append("<h3>" + object.description);
+        //     // $("#eventsDisplay").append("<br>");
+
+        //     $(".eventsDisplayStyle").append(emptyEventsContainer);
+        //     $(".eventTitle").append("<h2>" + object.title);
+        //     $(".eventDate").append("<h3>" + object.date);
+        //     $(".eventDescription").append("<h3>" + object.description);
+        //     $(".eventsDisplayStyle").append("<br>");
+        // }
 
         const displayData = data;
-        displayData.forEach((object) => {
-            $("#eventsDisplay").append("<br>");
-            $("#eventsDisplay").append("<h2>" + object.title);
-            $("#eventsDisplay").append("<h3>" + object.date);
-        });
-        //$("#eventTitle").append("<h1>" + data[0].title );
+
+        // displayData.forEach((object) => {
+        //     // $("#eventsDisplay").append("<br>");
+        //     // $("#eventsDisplay").append("<h2>" + object.title);
+        //     // $("#eventsDisplay").append("<h3>" + object.date);
+        //     // $("#eventsDisplay").append("<h3>" + object.description);
+        //     // $("#eventsDisplay").append("<br>");
+        //     showEvents(object);
+        //     // How add a css property dynamically
+        //     // $("#eventsDisplay").append("<h3>" + object.description).css("font-size", "50px");
+        // });
+
+        for (var i = 0; i < data.length; i++) {
+
+            var row = $("<div>");
+            row.addClass("event");
+
+            row.append("<p>" + data[i].title + " event.. </p>");
+            row.append("<p>" + data[i].date + "</p>");
+            // row.append("<p>At " + moment(data[i].created_at).format("h:mma on dddd") + "</p>");
+
+            $("#eventsDisplay").append(row);
+
+        }
+
 
         if (data) {
-            
-            $("#eventsDisplay").append(data);
-            console.log("testing this out", data);
+
+            // $("#eventsDisplay").append(data);
+            // console.log("testing this out", data);
 
         }
     });
