6874cfe6e7d887d319c02680aa509a09930edf5c
Chance Phiniezy <phiniezyc@gmail.com>
2017-12-03 19:37:16 -0500

    reverted back to tweet display.  Working

--- public/assets/js/showEvents.js
+++ public/assets/js/showEvents.js
@@ -1,62 +1,31 @@
 $(document).ready(function () {
 
-    const displayEventsSection = $("#eventTitle");
-
+    const displayEventsSection = $("#eventsDisplay");
 
     $.get("/api/events/", function (data) {
         //console.log(data.id);
-        $("#eventTitle").append("<h1>" + data[0].title);
+        $("#eventsDisplay").append("<h1>" + data[0].title);
 
         const displayData = data;
         displayData.forEach((object) => {
-            $("#eventTitle").append("<br>");
-            $("#eventTitle").append("<h2>" + object.title);
-            $("#eventTitle").append("<h3>" + object.date);
+            $("#eventsDisplay").append("<br>");
+            $("#eventsDisplay").append("<h2>" + object.title);
+            $("#eventsDisplay").append("<h3>" + object.date);
         });
         //$("#eventTitle").append("<h1>" + data[0].title );
 
         if (data) {
-            // If this post exists, prefill our cms forms with its data
-            //displayEventsSection.val(data.newEventNameInput);
-            //displayEventsSection.val(data.title);
-
-            //console.log(displayEventsSection.val(data.newEventNameInput));
-            //console.log(displayEventsSection.val(data.title));
-
-            //$("#displayEventsSection").html(data.title);
-            $("#eventTitle").append(data);
+            
+            $("#eventsDisplay").append(data);
             console.log("testing this out", data);
 
-
-            console.log(data[0].title);
-
-
-
-
-
-
-
-
-
-            // bodyInput.val(data.body);
-            // postCategorySelect.val(data.category);
-            // // If we have a post with this id, set a flag for us to know to update the post
-            // // when we hit submit
-            // updating = true;
         }
     });
 
     $("#goToAddEventsPageButton").on("click", ((event) => {
         event.preventDefault();
 
-
-
-
-
-
         window.location.replace('/userDashboard');
-
-
 
     }));
 
